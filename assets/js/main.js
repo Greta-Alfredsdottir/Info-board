bb43c9b8338dff9f5dcbf0247ad6a07f53aeb39f
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-05-28 09:00:23 +0000

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // her samle vi det hele
 
 import getSkema from './modules/skema/skema.js';
-import updateClock from './Modules/Ur/ur.js';
+import updateClock from './modules/Ur/ur.js';
 import hentData from "./modules/opslagstavle/opslagstalve.js";
 import startKantine from './modules/kantine/kantine.js';
 
